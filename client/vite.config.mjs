9f06426e8b48844a49fe8a0715cb3c37ed67e54e
Marcaida Algie M. <143990349+algie06@users.noreply.github.com>
2025-02-26 17:03:32 +0800

    aaaaa

--- client/vite.config.mjs
+++ client/vite.config.mjs
@@ -1,53 +1,3 @@
-// import { defineConfig } from 'vite'
-// import react from '@vitejs/plugin-react'
-// import path from 'node:path'
-// import autoprefixer from 'autoprefixer'
-
-// export default defineConfig(() => {
-//   return {
-//     base: './',
-//     build: {
-//       outDir: 'build',
-//       chunkSizeWarningLimit: 600,
-//     },
-//     css: {
-//       postcss: {
-//         plugins: [
-//           autoprefixer({}), // add options if needed
-//         ],
-//       },
-//     },
-//     esbuild: {
-//       loader: 'jsx',
-//       include: /src\/.*\.jsx?$/,
-//       exclude: [],
-//     },
-//     optimizeDeps: {
-//       esbuildOptions: {
-//         loader: {
-//           '.js': 'jsx',
-//         },
-//       },
-//     },
-//     plugins: [react()],
-//     resolve: {
-//       alias: [
-//         {
-//           find: 'src/',
-//           replacement: `${path.resolve(__dirname, 'src')}/`,
-//         },
-//       ],
-//       extensions: ['.mjs', '.js', '.ts', '.jsx', '.tsx', '.json', '.scss'],
-//     },
-//     server: {
-//       port: 5000,
-//       proxy: {
-//         // https://vitejs.dev/config/server-options.html
-//       },
-//     },
-//   }
-// })
-
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import path from 'node:path'
@@ -55,16 +5,16 @@ import autoprefixer from 'autoprefixer'
 
 export default defineConfig(() => {
   return {
-    base: '/',
-    logLevel: 'info',
-    clearScreen: false,
+    base: './',
     build: {
       outDir: 'build',
       chunkSizeWarningLimit: 600,
     },
     css: {
       postcss: {
-        plugins: [autoprefixer({})],
+        plugins: [
+          autoprefixer({}), // add options if needed
+        ],
       },
     },
     esbuild: {
@@ -73,7 +23,6 @@ export default defineConfig(() => {
       exclude: [],
     },
     optimizeDeps: {
-      force: true,
       esbuildOptions: {
         loader: {
           '.js': 'jsx',
@@ -92,7 +41,6 @@ export default defineConfig(() => {
     },
     server: {
       port: 5000,
-      host: true,
       proxy: {
         // https://vitejs.dev/config/server-options.html
       },
