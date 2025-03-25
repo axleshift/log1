e01321673d37065043605d95edbcdd69e53a5e18
freight-capstone <algie0506@gmail.com>
2025-03-25 17:11:12 +0000

    Production

---
+++ assets/DispatchingItems-BajRCtDq.js
@@ -0,0 +1,1 @@
+import{j as s}from"./index-CBxfVCnW.js";const t=()=>s.jsx(s.Fragment,{children:s.jsx("p",{})}),r=()=>s.jsxs(s.Fragment,{children:[s.jsx("h1",{children:"Dispatching Items"}),s.jsx(t,{})]});export{r as default};
