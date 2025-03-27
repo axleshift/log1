543b03ce44af72cd00d1bf8756410e8a33147046
freight-capstone <algie0506@gmail.com>
2025-03-27 19:55:02 +0000

    Production

---
+++ assets/DispatchingItems-CAMbsT2V.js
@@ -0,0 +1,1 @@
+import{j as s}from"./index-9q14rM7R.js";const t=()=>s.jsx(s.Fragment,{children:s.jsx("p",{})}),r=()=>s.jsxs(s.Fragment,{children:[s.jsx("h1",{children:"Dispatching Items"}),s.jsx(t,{})]});export{r as default};
