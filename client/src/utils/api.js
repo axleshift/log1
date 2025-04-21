b6cdad72c663667923b069aaa420498e5de2c1f7
Marcaida Algie M. <143990349+algie06@users.noreply.github.com>
2025-04-21 17:09:30 +0800

    bug in api interceptor

--- client/src/utils/api.js
+++ client/src/utils/api.js
@@ -1,17 +1,21 @@
 // import axios from 'axios'
 // const API_URL = import.meta.env.VITE_APP_API_URL
 // const API_KEY = import.meta.env.VITE_APP_API_KEY
+
 // const api = axios.create({
 //   baseURL: API_URL,
 //   headers: {
 //     'Content-Type': 'multipart/form-data' && 'application/json',
 //     'x-api-key': API_KEY,
 //   },
+//   withCredentials: true, // Enable credentials
 // })
 
+// // Add request interceptor
 // api.interceptors.request.use(
 //   (config) => {
-//     const token = sessionStorage.getItem('accessToken')
+//     // Change from sessionStorage to localStorage
+//     const token = localStorage.getItem('accessToken')
 //     if (token) {
 //       config.headers.Authorization = `Bearer ${token}`
 //     }
@@ -25,29 +29,44 @@
 // export default api
 
 import axios from 'axios'
+
 const API_URL = import.meta.env.VITE_APP_API_URL
 const API_KEY = import.meta.env.VITE_APP_API_KEY
 
 const api = axios.create({
   baseURL: API_URL,
   headers: {
-    'Content-Type': 'application/json' && 'multipart/form-data',
     'x-api-key': API_KEY,
   },
-  withCredentials: true, // Enable credentials
+  withCredentials: true,
+  timeout: 10000,
 })
 
-// Add request interceptor
 api.interceptors.request.use(
   (config) => {
-    // Change from sessionStorage to localStorage
     const token = localStorage.getItem('accessToken')
     if (token) {
       config.headers.Authorization = `Bearer ${token}`
     }
+
+    // Set Content-Type based on data type
+    if (config.data instanceof FormData) {
+      config.headers['Content-Type'] = 'multipart/form-data'
+    } else {
+      config.headers['Content-Type'] = 'application/json'
+    }
+
     return config
   },
+  (error) => Promise.reject(error),
+)
+
+api.interceptors.response.use(
+  (response) => response,
   (error) => {
+    if (error.response?.status === 401) {
+      localStorage.removeItem('accessToken')
+    }
     return Promise.reject(error)
   },
 )
